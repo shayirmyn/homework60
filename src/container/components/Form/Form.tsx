3b66b45b6a9ab98c9c490b655f8c6b69b543d959
Shayirbek Abdymomunov <legionera803@gmail.com>
2023-06-19 17:28:43 +0600

    get request is redone from axios request to fetch request

--- src/container/components/Form/Form.tsx
+++ src/container/components/Form/Form.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 const Form = () => {
+
+
+
     return (
         <form className="form">
            <div className="innerForm">
--- src/container/components/Get/Get.tsx
+++ src/container/components/Get/Get.tsx
@@ -1,7 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import axios from 'axios';
 import {IMessages} from "../../../types.d";
-
 const url = "http://146.185.154.90:8000/messages";
 
 const Get = () => {
@@ -9,27 +7,43 @@ const Get = () => {
     const [messages, setMessages] = useState<IMessages[]>([]);
 
     const fetchData = async () => {
-        const responce = await axios.get(url);
-        setMessages(responce.data);
+        const responce = await fetch(url);
+        const parseResponce = await responce.json();
+        setMessages(parseResponce);
     };
 
     useEffect(() => {
         fetchData().catch(error => console.error(error));
-    }, []);
+        if (messages.length > 0) {
+            let lastDateTime = messages[messages.length - 1].datetime;
+            setInterval(async () => {
+                const dateTimeRequest = await fetch(`${url}?datetime=${lastDateTime}`);
+                const parseDateTimeRequest = await dateTimeRequest.json();
 
-    console.log(messages);
+                if (parseDateTimeRequest.length > 0) {
+                    setMessages( prevState => [...prevState, parseDateTimeRequest]);
+                } else {
+                    console.log("nothing!!!")
+                }
+
+                lastDateTime = parseDateTimeRequest[parseDateTimeRequest.length - 1].datetime;
+            }, 3000);
+        }
+    }, [messages]);
 
     return (
         <div className="container">
             <div className="container2">
                 <div>
-                    {messages.map((message: IMessages) => (
-                        <div className="divInfo">
-                            <p className="authorName"><strong>author:</strong> {message.author}</p>
-                            <p className="messageInfo"><strong>message:</strong> {message.message}</p>
-                            <p className="dateInfo"><strong>date:</strong> {message.datetime}</p>
-                        </div>
-                    ))}
+                    {
+                        messages.length < 1 ? <h5>Please send something to show..</h5> :
+                            messages.map((message: IMessages) => (
+                                <div className="divInfo" key={message.datetime}>
+                                    <p className="authorName"><strong>author:</strong> {message.author}</p>
+                                    <p className="messageInfo"><strong>message:</strong> {message.message}</p>
+                                    <p className="dateInfo"><strong>date:</strong> {message.datetime}</p>
+                                </div>
+                            ))}
                 </div>
             </div>
         </div>
